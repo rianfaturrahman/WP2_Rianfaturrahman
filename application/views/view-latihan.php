51ea31db1c703ecb5016211ef00ec8d17397a424
rianfaturrahman <rianfaturahman.rf2@gmail.com>
2023-10-10 13:09:48 +0700

    Latihan p3 dan p2

---
+++ application/views/view-latihan.php
@@ -0,0 +1,12 @@
+<html>
+    <head>
+        <title>Latihan 1</title>
+</head>
+
+<body>
+    Halo kawan.. yuk kita belajar web programing.. !!<br>
+    Nilai 1 =<?= $nilai1; ?>
+    Nilai 2 =<?= $nilai2; ?>
+    ini hasil dari pemodelan dengan method penjumlahan yaitu <?=$nilai1 ."+". $nilai2."=". $hasil; ?>
+</body>
+</html>
